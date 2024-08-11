b277840a780694ffd4b59b0de17536783cb53a23
namay10 <namaygupta101201@gmail.com>
2024-08-11 21:35:10 +0530

    update

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,8 +5,28 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'primary-color' : '#34495E',
+       //'primary-color' :'#73AFD2',
+        'secondary-color' : '#05c3ff',
+        'highlight-color' : '#F8F8FF',
+        'hover-color' : '#0058e0',
+        'header-hover-color': '#589bff',
+         'bgcolor':"#DAE0E6",
+        //'text-color' : '#373234',
+      },
+      width: {
+        '96': '24rem', 
+        '112': '32rem',
+        '128': '40rem',
+           },
+      height: {
+        '96': '24rem',
+        '112': '32rem',
+        '128': '40rem',
+       },
+    },
   },
   plugins: [],
 }
-
